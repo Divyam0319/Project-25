e29d19ac1946cabe23e3deac45c5504192790f98
Divyam0319 <75788540+Divyam0319@users.noreply.github.com>
2021-01-29 11:51:53 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+var paper;
+
+
+function setup() {
+	createCanvas(1600, 990);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	paper=new Paper(150,520,70);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  paper.display();
+
+}
+
+function keyPressed(){
+
+	if(keyDown("up_arrow")){
+
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:430,y:731});
+		
+	}
+	
+
+}
